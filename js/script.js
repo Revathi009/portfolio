cb40c837341ed92467c3651068c6d096ce0f447a
Revathi009 <revathidusa009@gmail.com>
2021-11-20 21:14:48 +0530

    Home section including media queries is done

---
+++ js/script.js
@@ -0,0 +1,13 @@
+(function($){
+
+    var typed = new Typed('span.txt-rotate', {
+        strings: ["I'm a Web Developer", "I'm a Business Analyst", "I'm a Microsoft certified Expert"],
+        typeSpeed: 100,
+        backSpeed: 100,
+        fadeOut: false,
+        smartBackspace: true,
+        loop: true
+    });
+
+})(jQuery);
+   
