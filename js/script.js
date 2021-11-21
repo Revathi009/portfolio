70a0fd96740fb91db4f238901ca0f88da116857a
Revathi009 <revathidusa009@gmail.com>
2021-11-21 17:25:25 +0530

    Work section completed

--- js/script.js
+++ js/script.js
@@ -10,4 +10,32 @@
     });
 
 })(jQuery);
+
+$(document).ready(function(){
+    $('.projects-wrapper').isotope({
+        filter: '*',
+        layoutMode: 'masonry'
+    });
+
+    $('.list-filter a').click(function(){
+        var selector = $(this).attr('data-filter');
+        $('.list-filter a').removeClass('active');
+        $(this).addClass('active');
+        $('.projects-wrapper').isotope({
+            filter: selector
+        });
+        return false;
+    });
+
+    $('.popup-image').magnificPopup({
+        type: 'image',
+        closeOnContentClick: true,
+        gallery: {
+            enabled: true,
+            navigateByImgClick: true
+        }
+    });
+});
+
+
    
