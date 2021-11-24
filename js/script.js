cb23ab9ba42afa2d49e2801d0b949a13024fcbc4
Revathi009 <revathidusa009@gmail.com>
2021-11-24 18:08:04 +0530

    about me page and testimonals section is done

--- js/script.js
+++ js/script.js
@@ -9,6 +9,32 @@
         loop: true
     });
 
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: false,
+        dots: true,
+        items: 2,
+        autoWidth: false,
+        center: false,
+        mergeFit: true,
+        responsive : {
+            0: {
+                items: 1,
+                nav: false
+            },
+            576: {
+                items: 1,
+                nav: false
+            },
+            768: {
+                items: 2,
+                nav: false
+            }
+        }
+    })
+
+
 })(jQuery);
 
 $(document).ready(function(){
